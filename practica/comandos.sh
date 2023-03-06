625c55108a20900bba95610bd8b625eac0c385ab
Roberto Rodríguez Márquez <krowno.iso@gmail.com>
2023-03-06 08:42:13 +0100

    subidos comandos que crean la clave y los usuarios de mariadb

---
+++ practica/comandos.sh
@@ -0,0 +1,5 @@
+kubectl create cm cm-mariadb --from-literal=mysql_usuario=bookmedik     \
+                            --from-literal=basededatos=bookmedik
+
+kubectl create secret generic secret-mariadb --from-literal=password=bookmedik   \
+                                            --from-literal=rootpass=root
